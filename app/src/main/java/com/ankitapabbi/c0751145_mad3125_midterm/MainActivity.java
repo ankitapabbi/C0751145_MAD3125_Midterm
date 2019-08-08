c49bd5903b52cfc11a3937c972c023dc70d392cb
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-08 18:40:43 -0400

    [Ankita Pabbi] Mid Term Project Android.

---
+++ app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ankitapabbi.c0751145_mad3125_midterm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
