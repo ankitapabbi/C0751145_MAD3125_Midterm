ca03d3e9c683df549840d2fc7b3b6a4198531796
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-09 15:39:01 -0400

    [Ankita Pabbi] Updated Detail Screen , Sent data through intent in order to display on detail information.

--- app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/DetailScreen.java
+++ app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/DetailScreen.java
@@ -1,13 +1,36 @@
 package com.ankitapabbi.c0751145_mad3125_midterm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailScreen extends AppCompatActivity {
 
+    ImageView mPatch;
+    TextView missionName,launchYear,launchDateUnix,launchDateUtc,rocketId,rocketName,rocketType,articallink;
+    String wikiLink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_screen);
+        
+
+        Intent intent = getIntent();
+
+        intent.getStringExtra("flight_number");
+        intent.getStringExtra("mission_name");
+        intent.getStringExtra("launch_year");
+        intent.getStringExtra("launch_date_unix");
+        intent.getStringExtra("launch_date_utc");
+        intent.getStringExtra("rocket_id");
+        intent.getStringExtra("rocket_name");
+        intent.getStringExtra("rocket_type");
+        intent.getStringExtra("article_link");
+        intent.getStringExtra("wikipedia");
+        intent.getStringExtra("mission_patch");
+
+
     }
 }
