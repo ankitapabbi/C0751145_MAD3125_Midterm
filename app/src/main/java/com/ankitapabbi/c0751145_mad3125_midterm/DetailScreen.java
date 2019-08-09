4c23869592a80e4f35fe06c5b026a12e50c3bc54
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-09 16:00:18 -0400

    [Ankita Pabbi] Updated Detail Screen ,modifying its layout.

--- app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/DetailScreen.java
+++ app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/DetailScreen.java
@@ -1,35 +1,55 @@
 package com.ankitapabbi.c0751145_mad3125_midterm;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailScreen extends AppCompatActivity {
 
     ImageView mPatch;
-    TextView missionName,launchYear,launchDateUnix,launchDateUtc,rocketId,rocketName,rocketType,articallink;
+    TextView missionName,launchYear,launchDateUnix,launchDateUtc,rocketId,rocketName,rocketType,articallink,flightNumber;
     String wikiLink;
+    FloatingActionButton btninfo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_screen);
-        
+        mPatch = (ImageView)findViewById(R.id.mPatch);
+        missionName = (TextView)findViewById(R.id.mission);
+        launchYear = (TextView)findViewById(R.id.missionYear);
+        launchDateUnix = (TextView)findViewById(R.id.launchDate1);
+        launchDateUtc = (TextView)findViewById(R.id.launchDate2);
+        rocketId = (TextView)findViewById(R.id.rocketId);
+        rocketName = (TextView)findViewById(R.id.rocketName);
+        rocketType = (TextView)findViewById(R.id.rocketType);
+        articallink = (TextView)findViewById(R.id.arcticalLink);
+        flightNumber = (TextView)findViewById(R.id.flightNumber);
+        btninfo = (FloatingActionButton) findViewById(R.id.btninfo);
+
+
+
+
+
 
         Intent intent = getIntent();
 
-        intent.getStringExtra("flight_number");
-        intent.getStringExtra("mission_name");
-        intent.getStringExtra("launch_year");
-        intent.getStringExtra("launch_date_unix");
-        intent.getStringExtra("launch_date_utc");
-        intent.getStringExtra("rocket_id");
-        intent.getStringExtra("rocket_name");
-        intent.getStringExtra("rocket_type");
-        intent.getStringExtra("article_link");
-        intent.getStringExtra("wikipedia");
-        intent.getStringExtra("mission_patch");
+        flightNumber.setText(intent.getStringExtra("flight_number"));
+        missionName.setText(intent.getStringExtra("mission_name"));
+        launchYear.setText(intent.getStringExtra("launch_year"));
+        launchDateUnix.setText(intent.getStringExtra("launch_date_unix"));
+        launchDateUtc.setText(intent.getStringExtra("launch_date_utc"));
+        rocketId.setText(intent.getStringExtra("rocket_id"));
+        rocketName.setText(intent.getStringExtra("rocket_name"));
+        rocketType.setText(intent.getStringExtra("rocket_type"));
+        articallink.setText(intent.getStringExtra("article_link"));
+        wikiLink = intent.getStringExtra("wikipedia");
+        Glide.with(getApplicationContext()).load(intent.getStringExtra("mission_patch")).into(mPatch);
 
 
     }
