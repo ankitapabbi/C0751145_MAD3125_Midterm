87a7e21c6f44bc216f99e40b8febf7469860c674
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-09 02:30:19 -0400

    [Ankita Pabbi] Created Model for mission data with appropriate variables.

--- app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/HomeScreen.java
+++ app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/HomeScreen.java
@@ -2,12 +2,16 @@ package com.ankitapabbi.c0751145_mad3125_midterm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class HomeScreen extends AppCompatActivity {
 
+    RecyclerView dataRecyler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        dataRecyler = (RecyclerView)findViewById(R.id.dataRecyler);
+
     }
 }
---
+++ app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/Model/MissionData.java
@@ -0,0 +1,106 @@
+package com.ankitapabbi.c0751145_mad3125_midterm.Model;
+
+public class MissionData {
+
+
+    private String flight_number;
+    private String mission_name;
+    private String launch_year;
+    private String launch_date_unix;
+    private String launch_date_utc;
+    private String rocket_id;
+    private String rocket_name;
+    private String rocket_type;
+    private String article_link;
+    private String wikipedia;
+    private String mission_patch;
+
+
+    public String getFlight_number() {
+        return flight_number;
+    }
+
+    public void setFlight_number(String flight_number) {
+        this.flight_number = flight_number;
+    }
+
+    public String getMission_name() {
+        return mission_name;
+    }
+
+    public void setMission_name(String mission_name) {
+        this.mission_name = mission_name;
+    }
+
+    public String getLaunch_year() {
+        return launch_year;
+    }
+
+    public void setLaunch_year(String launch_year) {
+        this.launch_year = launch_year;
+    }
+
+    public String getLaunch_date_unix() {
+        return launch_date_unix;
+    }
+
+    public void setLaunch_date_unix(String launch_date_unix) {
+        this.launch_date_unix = launch_date_unix;
+    }
+
+    public String getLaunch_date_utc() {
+        return launch_date_utc;
+    }
+
+    public void setLaunch_date_utc(String launch_date_utc) {
+        this.launch_date_utc = launch_date_utc;
+    }
+
+    public String getRocket_id() {
+        return rocket_id;
+    }
+
+    public void setRocket_id(String rocket_id) {
+        this.rocket_id = rocket_id;
+    }
+
+    public String getRocket_name() {
+        return rocket_name;
+    }
+
+    public void setRocket_name(String rocket_name) {
+        this.rocket_name = rocket_name;
+    }
+
+    public String getRocket_type() {
+        return rocket_type;
+    }
+
+    public void setRocket_type(String rocket_type) {
+        this.rocket_type = rocket_type;
+    }
+
+    public String getArticle_link() {
+        return article_link;
+    }
+
+    public void setArticle_link(String article_link) {
+        this.article_link = article_link;
+    }
+
+    public String getWikipedia() {
+        return wikipedia;
+    }
+
+    public void setWikipedia(String wikipedia) {
+        this.wikipedia = wikipedia;
+    }
+
+    public String getMission_patch() {
+        return mission_patch;
+    }
+
+    public void setMission_patch(String mission_patch) {
+        this.mission_patch = mission_patch;
+    }
+}
