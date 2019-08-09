e91fd207f8b2085a2c5d4d9fd06b5ee097780f91
Ankita Pabbi <erankitapabbi@gmail.com>
2019-08-09 17:19:24 -0400

    [Ankita Pabbi] Added animation to the layouts for the Splash.

--- app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/MainActivity.java
+++ app/src/main/java/com/ankitapabbi/c0751145_mad3125_midterm/MainActivity.java
@@ -1,15 +1,42 @@
 package com.ankitapabbi.c0751145_mad3125_midterm;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    LinearLayout l1,l2;
+
+    Animation uptodown,downtoup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        l1=(LinearLayout)findViewById(R.id.l1);
+        l2=(LinearLayout)findViewById(R.id.l2);
 
+        uptodown= AnimationUtils.loadAnimation(this,R.anim.uptodown);
+        downtoup= AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        l1.setAnimation(uptodown);
+        l2.setAnimation(downtoup);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
 
     }
 }
